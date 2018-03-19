1a39c522eaccd54aaa3cb13bd241a5f7fed63af8
Carlos Eduardo Alves <kmiksi@gmail.com>
2018-03-19 19:04:49 -0300

    navigation

--- app/View/Cliente/index.php
+++ app/View/Cliente/index.php
@@ -33,17 +33,19 @@
         <?php } ?>
         </tbody>
     </table>
-    <nav aria-label="navigation">
-        <ul class="pagination justify-content-end">
-            <li class="page-item disabled">
-                <a class="page-link" href="#" tabindex="-1">Previous</a>
-            </li>
-            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item">
-                <a class="page-link" href="#">Next</a>
-            </li>
-        </ul>
-    </nav>
+    <?php if ($navigation['pages'] > 1) { ?>
+        <nav aria-label="navigation">
+            <ul class="pagination justify-content-end">
+                <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1">Previous</a>
+                </li>
+                <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                </li>
+            </ul>
+        </nav>
+    <?php } ?>
 </div>
